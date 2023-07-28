2111947a729443467e0b958caa1e59e1a6852621
simphiwe-nkabinde <simphiwe.kabini@gmail.com>
2023-07-28 13:31:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const { setCollectionsData, getItemIds, getPermissionIds, strapiUpdate } = require('./utils')
+require('dotenv').config()
+const app = express()
+
+app.use(express.json());
+
+let collectionsData = {}
+setCollectionsData().then(data => { collectionsData = data })
+
+app.post('/update', (req, res) => {
+    const microapp = req.body.attributes
+    const microappId = req.body.id
+    delete microapp.icon //if icon is not removed, strapi will remove existing icon image
+
+    microapp.languages_ = getItemIds(microapp.languages[0].name, collectionsData.languages.data)
+    microapp.categories_ = getItemIds(microapp.category, collectionsData.categories.data)
+    microapp.permissions_ = getPermissionIds(microapp, collectionsData.permissions.data)
+    microapp.countries_ = getItemIds(microapp.countries[0].name, collectionsData.countries.data)
+
+    res.status(200).json(microapp)
+    strapiUpdate(microappId, microapp, token)
+        .then(data => res.status(200))
+        .catch(err => res.status(400))
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+})
---
+++ utils.js
@@ -0,0 +1,71 @@
+const axios = require('axios')
+require('dotenv').config()
+
+const baseUrl = process.env.STRAPI_URL;
+
+exports.setCollectionsData = async function () {
+    const collectionData = {}
+    collectionData.categories = await fetchQuery("api/categories");
+    collectionData.languages = await fetchQuery("api/language-items");
+    collectionData.permissions = await fetchQuery("api/permission-items");
+    collectionData.countries = await fetchQuery("api/countries");
+    return collectionData
+}
+
+const fetchQuery = async function (path) {
+    let url = `${baseUrl}/${path}`;
+    return await axios
+        .get(url)
+        .then((data) => data.data)
+        .catch((err) => err.message);
+}
+/**
+ * returns collection items IDs. 
+ * returned value is an array of language IDs that match selected languages
+ * @param {string} selectedItems JSON Array
+ * @param {[{id: number, attributes: {name: string}}]} collectionData 
+ * @returns {[number]}
+ */
+exports.getItemIds = function (selectedItems, collectionData) {
+    if (typeof selectedItems !== 'string') return []
+    if (!selectedItems.length) return []
+    if (!Array.isArray(collectionData)) return []
+    if (!collectionData.length) return []
+
+    const list = collectionData.filter(item => {
+        if (selectedItems.toLocaleLowerCase().includes(item.attributes.name.toLocaleLowerCase())) return item.id
+    })
+    return list.map(item => item.id)
+}
+/**
+ * return selected permission ids
+ * returned value is an array of permission IDs that match selected permissions
+ * @param {{}} microappInfo microapp data
+ * @param {[{id: number, name: string}]} permissionData 
+ * @returns {[number]}
+ */
+exports.getPermissionIds = function (microappInfo, permissionData) {
+    if (typeof microappInfo !== 'object') return []
+    if (!Array.isArray(permissionData)) return []
+    if (!permissionData.length) return []
+
+    const list = []
+    permissionData.forEach(item => {
+        if (microappInfo[item.attributes.name] === true) {
+            list.push(item.id)
+        }
+    })
+    return list
+}
+exports.strapiUpdate = async function (id, microapp, token) {
+    if (!id) return
+    if (!microapp) return
+    if (!token) return
+
+    return await axios
+        .put(`${baseUrl}/api/micro-apps/${id}`, {data:microapp}, {
+            headers: { Authorization: `Bearer ${token}` },
+        })
+        .then((data) => {return Promise.resolve(data.data)})
+        .catch((err) => {return Promise.reject(err)});
+}
