3d69dd48feb9ba8be3de699d9f5aa8214156f28a
dfunani <dfunani@live.co.za>
2023-08-09 21:14:47 +0200

    addedlifecycle manager

--- app.js
+++ app.js
@@ -1,28 +1,69 @@
-const express = require('express')
-const { setCollectionsData, getItemIds, getPermissionIds, strapiUpdate } = require('./utils')
-require('dotenv').config()
-const app = express()
+const express = require("express");
+const {
+  setCollectionsData,
+  getItemIds,
+  getPermissionIds,
+  strapiUpdate,
+  setMicroAppState,
+} = require("./utils");
+require("dotenv").config();
+const path = require("path");
+const axios = require("axios");
+
+const app = express();
 
 app.use(express.json());
 
-let collectionsData = {}
-setCollectionsData().then(data => { collectionsData = data })
+let collectionsData = {};
+setCollectionsData().then((data) => {
+  collectionsData = data;
+});
 
-app.post('/update', (req, res) => {
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/app.html"));
+});
+
+app.post("/update", async (req, res) => {
+//   const dummy = await axios.get(
+//     process.env.STRAPI_URL + "/api/micro-apps/1040/?populate=*"
+//   );
+//   const microapp = dummy.data.data.attributes;
+//   const microappId = dummy.data.data.id;
     const microapp = req.body.attributes
     const microappId = req.body.id
-    delete microapp.icon //if icon is not removed, strapi will remove existing icon image
+  delete microapp.icon; //if icon is not removed, strapi will remove existing icon image
 
-    microapp.languages_ = getItemIds(microapp.languages[0].name, collectionsData.languages.data)
-    microapp.categories_ = getItemIds(microapp.category, collectionsData.categories.data)
-    microapp.permissions_ = getPermissionIds(microapp, collectionsData.permissions.data)
-    microapp.countries_ = getItemIds(microapp.countries[0].name, collectionsData.countries.data)
+  microapp.languages_ = getItemIds(
+    microapp.languages[0].name,
+    collectionsData.languages.data
+  );
+  microapp.categories_ = getItemIds(
+    microapp.category,
+    collectionsData.categories.data
+  );
+  microapp.permissions_ = getPermissionIds(
+    microapp,
+    collectionsData.permissions.data
+  );
+  microapp.countries_ = getItemIds(
+    microapp.countries[0].name,
+    collectionsData.countries.data
+  );
 
-    strapiUpdate(microappId, microapp, token)
-        .then(data => res.status(200))
-        .catch(err => res.status(400))
-})
+  microapp.lifecycle = await setMicroAppState(microapp, microappId);
+  
+  const data = {
+    languages_: [...microapp.languages_],
+    categories_: [...microapp.categories_],
+    permissions_: [...microapp.permissions_],
+    countries_: [...microapp.countries_],
+    lifecycle: [...microapp.lifecycle],
+  };
+  strapiUpdate(microappId, data)
+    .then((data) => res.status(200).json({ ...data }))
+    .catch((err) => res.status(400).json({ ...err }));
+});
 
 app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
-})
+  console.log(`Example app listening on port ${process.env.PORT}`);
+});
